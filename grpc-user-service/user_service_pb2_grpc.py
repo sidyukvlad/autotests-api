0a0d9e403fc486397f1b86fa6ab1ed54bce83a33
Sidyuk Vlad <sidyukvlad@gmail.com>
2025-09-13 18:52:34 +0300

    grpc client + server for task

--- grpc-user-service/user_service_pb2_grpc.py
+++ grpc-user-service/user_service_pb2_grpc.py
@@ -5,23 +5,26 @@ import warnings
 
 import user_service_pb2 as user__service__pb2
 
-GRPC_GENERATED_VERSION = '1.74.0'
+GRPC_GENERATED_VERSION = "1.74.0"
 GRPC_VERSION = grpc.__version__
 _version_not_supported = False
 
 try:
     from grpc._utilities import first_version_is_lower
-    _version_not_supported = first_version_is_lower(GRPC_VERSION, GRPC_GENERATED_VERSION)
+
+    _version_not_supported = first_version_is_lower(
+        GRPC_VERSION, GRPC_GENERATED_VERSION
+    )
 except ImportError:
     _version_not_supported = True
 
 if _version_not_supported:
     raise RuntimeError(
-        f'The grpc package installed is at version {GRPC_VERSION},'
-        + f' but the generated code in user_service_pb2_grpc.py depends on'
-        + f' grpcio>={GRPC_GENERATED_VERSION}.'
-        + f' Please upgrade your grpc module to grpcio>={GRPC_GENERATED_VERSION}'
-        + f' or downgrade your generated code using grpcio-tools<={GRPC_VERSION}.'
+        f"The grpc package installed is at version {GRPC_VERSION},"
+        + f" but the generated code in user_service_pb2_grpc.py depends on"
+        + f" grpcio>={GRPC_GENERATED_VERSION}."
+        + f" Please upgrade your grpc module to grpcio>={GRPC_GENERATED_VERSION}"
+        + f" or downgrade your generated code using grpcio-tools<={GRPC_VERSION}."
     )
 
 
@@ -35,10 +38,11 @@ class UserServiceStub(object):
             channel: A grpc.Channel.
         """
         self.GetUser = channel.unary_unary(
-                '/userservice.UserService/GetUser',
-                request_serializer=user__service__pb2.GetUserRequest.SerializeToString,
-                response_deserializer=user__service__pb2.GetUserResponse.FromString,
-                _registered_method=True)
+            "/userservice.UserService/GetUser",
+            request_serializer=user__service__pb2.GetUserRequest.SerializeToString,
+            response_deserializer=user__service__pb2.GetUserResponse.FromString,
+            _registered_method=True,
+        )
 
 
 class UserServiceServicer(object):
@@ -47,43 +51,48 @@ class UserServiceServicer(object):
     def GetUser(self, request, context):
         """Missing associated documentation comment in .proto file."""
         context.set_code(grpc.StatusCode.UNIMPLEMENTED)
-        context.set_details('Method not implemented!')
-        raise NotImplementedError('Method not implemented!')
+        context.set_details("Method not implemented!")
+        raise NotImplementedError("Method not implemented!")
 
 
 def add_UserServiceServicer_to_server(servicer, server):
     rpc_method_handlers = {
-            'GetUser': grpc.unary_unary_rpc_method_handler(
-                    servicer.GetUser,
-                    request_deserializer=user__service__pb2.GetUserRequest.FromString,
-                    response_serializer=user__service__pb2.GetUserResponse.SerializeToString,
-            ),
+        "GetUser": grpc.unary_unary_rpc_method_handler(
+            servicer.GetUser,
+            request_deserializer=user__service__pb2.GetUserRequest.FromString,
+            response_serializer=user__service__pb2.GetUserResponse.SerializeToString,
+        ),
     }
     generic_handler = grpc.method_handlers_generic_handler(
-            'userservice.UserService', rpc_method_handlers)
+        "userservice.UserService", rpc_method_handlers
+    )
     server.add_generic_rpc_handlers((generic_handler,))
-    server.add_registered_method_handlers('userservice.UserService', rpc_method_handlers)
+    server.add_registered_method_handlers(
+        "userservice.UserService", rpc_method_handlers
+    )
 
 
- # This class is part of an EXPERIMENTAL API.
+# This class is part of an EXPERIMENTAL API.
 class UserService(object):
     """Missing associated documentation comment in .proto file."""
 
     @staticmethod
-    def GetUser(request,
-            target,
-            options=(),
-            channel_credentials=None,
-            call_credentials=None,
-            insecure=False,
-            compression=None,
-            wait_for_ready=None,
-            timeout=None,
-            metadata=None):
+    def GetUser(
+        request,
+        target,
+        options=(),
+        channel_credentials=None,
+        call_credentials=None,
+        insecure=False,
+        compression=None,
+        wait_for_ready=None,
+        timeout=None,
+        metadata=None,
+    ):
         return grpc.experimental.unary_unary(
             request,
             target,
-            '/userservice.UserService/GetUser',
+            "/userservice.UserService/GetUser",
             user__service__pb2.GetUserRequest.SerializeToString,
             user__service__pb2.GetUserResponse.FromString,
             options,
@@ -94,4 +103,5 @@ class UserService(object):
             wait_for_ready,
             timeout,
             metadata,
-            _registered_method=True)
+            _registered_method=True,
+        )
