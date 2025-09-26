a7f64b1e30c15413d88e9ab3a74db239d4199a11
Sidyuk Vlad <sidyukvlad@gmail.com>
2025-09-26 14:52:01 +0300

    7.1 Знакомство с Pydantic

--- pydantic_basics.py
+++ pydantic_basics.py
@@ -1,3 +1,5 @@
+from dataclasses import Field
+
 arr = {
   "id": "string",
   "title": "string",
@@ -7,16 +9,36 @@ arr = {
   "estimatedTime": "string"
 }
 
-from pydantic import BaseModel,Field
-# from pydantic.alias_generators import to_camel
+import uuid
+from pydantic import BaseModel, Field, EmailStr, HttpUrl, ValidationError
+from pydantic.alias_generators import to_camel
+
+
+class FileSchema(BaseModel):
+    id: str
+    url: str
+    filename: str
+    directory: str
+
+
+class UserSchema(BaseModel):
+    id: str
+    email: str
+    last_name: str = Field(alias="lastName")
+    first_name: str = Field(alias="firstName")
+    middle_name: str = Field(alias="middleName")
+
 
 class CourseSchema(BaseModel):
-    id: str
-    title: str
-    max_score: int = Field(alias="maxScore")
-    min_score: int = Field(alias="minScore")
-    description: str
-    estimated_time: str = Field(alias="estimatedTime")
+    # model_config = ConfigDict(alias_generator=to_camel, populate_by_name=True)
+    id: str = Field(default_factory=lambda: str(uuid.uuid4()))
+    title: str = "Title"
+    max_score: int = Field(alias="maxScore", default=100)
+    min_score: int = Field(alias="minScore", default=10)
+    description: str = "course"
+    preview_file: FileSchema = Field(alias="previewFile")
+    estimated_time: str = Field(alias="estimatedTime", default="2 weeks")
+    created_by_user: UserSchema = Field(alias="createdByUser")
 
 course_default_model = CourseSchema(
     id = "course-id",
@@ -24,36 +46,88 @@ course_default_model = CourseSchema(
     maxScore = 100,
     minScore = 10,
     description = "course-description",
-    estimatedTime = "1 week"
+    previewFile = FileSchema(
+        id = "preview-file",
+        url = "http://localhost:8000/preview-file",
+        filename = "file.png",
+        directory = "courses",
+    ),
+    estimatedTime = "1 week",
+    createdByUsser = UserSchema(
+        id="user-id",
+        email="user@gmail.com",
+        last_name="last-name",
+        first_name="first-name",
+        middle_name="middle-name",
+    )
 )
 
 print("course_default_model:", course_default_model)
 
+# Инициализируем модель CourseSchema через распаковку словаря
 course_dict = {
     "id": "course-id",
-    "title": "course-title",
+    "title": "Playwright",
     "maxScore": 100,
     "minScore": 10,
-    "description": "course-description",
-    "estimatedTime": "1 week"
+    "description": "Playwright",
+    # Добавили ключ previewFile
+    "previewFile": {
+        "id": "file-id",
+        "url": "http://localhost:8000",
+        "filename": "file.png",
+        "directory": "courses"
+    },
+    "estimatedTime": "1 week",
+    # Добавили ключ createdByUser
+    "createdByUser": {
+        "id": "user-id",
+        "email": "user@gmail.com",
+        "lastName": "Bond",
+        "firstName": "Zara",
+        "middleName": "Alise"
+    }
 }
-
 course_dict_model = CourseSchema(**course_dict)
+print('Course dict model:', course_dict_model)
+print(course_dict_model.model_dump())
+print(course_dict_model.model_dump(by_alias=True))
 
-print("course_dict_model:", course_dict_model)
-
+# Инициализируем модель CourseSchema через JSON
 course_json = """
 {
     "id": "course-id",
-    "title": "course-title",
-    "maxScore": 100000,
+    "title": "Playwright",
+    "maxScore": 100,
     "minScore": 10,
-    "description": "course-description",
-    "estimatedTime": "1 week"
+    "description": "Playwright",
+    "previewFile": {
+        "id": "file-id",
+        "url": "http://localhost:8000",
+        "filename": "file.png",
+        "directory": "courses"
+    },
+    "estimatedTime": "1 week",
+    "createdByUser": {
+        "id": "user-id",
+        "email": "user@gmail.com",
+        "lastName": "Bond",
+        "firstName": "Zara",
+        "middleName": "Alise"
+    }
 }
 """
-
 course_json_model = CourseSchema.model_validate_json(course_json)
-print("course_json_model:", course_json_model)
-print(course_json_model.model_dump())
-print(course_json_model.model_dump_json(by_alias=True))
+print('Course JSON model:', course_json_model)
+
+# Инициализируем FileSchema c некорректным url
+try:
+    file = FileSchema(
+        id="file-id",
+        url="localhost",
+        filename="file.png",
+        directory="courses",
+    )
+except ValidationError as error:
+    print(error)
+    print(error.errors())
