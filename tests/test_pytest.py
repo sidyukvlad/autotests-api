39bb81d2f8977acee36430e659e1feebe94ad94d
Sidyuk Vlad <sidyukvlad@gmail.com>
2025-10-04 22:55:00 +0300

    8.1 Введение в Pytest

---
+++ tests/test_pytest.py
@@ -0,0 +1,21 @@
+def test_user_login():
+    pass
+
+def test_first_try():
+    print("hello")
+
+def test_create_user():
+    print("hello")
+
+def test_assert_positive_case():
+    assert (2 + 2) == 4
+
+def test_assert_negative_case():
+    x, y = 5, 2
+    assert (x + y) == 5
+
+class TestUserAuthentification:
+    def test_create_user(self):
+        pass
+    def test_update_user(self):
+        pass
