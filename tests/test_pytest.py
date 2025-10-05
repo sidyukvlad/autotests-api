0c0fa50b24df3a1391402d1cc707be8ca3e01357
Sidyuk Vlad <sidyukvlad@gmail.com>
2025-10-06 01:13:14 +0300

    8.2 Написание API автотестов используя Pytest

--- tests/test_pytest.py
+++ tests/test_pytest.py
@@ -1,3 +1,5 @@
+import pytest
+
 def test_user_login():
     pass
 
---
+++ tools/assertions/base.py
@@ -0,0 +1,24 @@
+from typing import Any
+
+
+def  assert_status_code(actual: int, expected: int):
+    assert actual == expected, (
+        f'Incorrect response status code. '
+        f'Expected status code: {expected}. '
+        f'Actual status code: {actual}'
+    )
+
+def assert_equal(actual: Any, expected: Any, name: str):
+    """
+    Проверяет, что фактическое значение равно ожидаемому.
+
+    :param name: Название проверяемого значения.
+    :param actual: Фактическое значение.
+    :param expected: Ожидаемое значение.
+    :raises AssertionError: Если фактическое значение не равно ожидаемому.
+    """
+    assert actual == expected, (
+        f'Incorrect value: "{name}". '
+        f'Expected value: {expected}. '
+        f'Actual value: {actual}'
+    )
