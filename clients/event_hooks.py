2eece6a3ff9a9338bbfc77b9c5797c3a03258c65
Sidyuk Vlad <sidyukvlad@gmail.com>
2025-10-29 23:24:31 +0300

    11.3 Добавляем логирование для API автотестов

--- clients/event_hooks.py
+++ clients/event_hooks.py
@@ -18,8 +18,18 @@ def curl_event_hook(request: Request):
 
 
 def log_request_event_hook(request: Request):
+    """
+    Логирует информацию об отправленном HTTP-запросе.
+
+    :param request: Объект запроса HTTPX.
+    """
     logger.info(f"Make {request.method} request to {request.url}")
 
 
 def log_response_event_hook(response: Response):
+    """
+    Логирует информацию о полученном HTTP-ответе.
+
+    :param response: Объект ответа HTTPX.
+    """
     logger.info(f"Got response  {response.status_code} {response.reason_phrase} from {response.url}")
